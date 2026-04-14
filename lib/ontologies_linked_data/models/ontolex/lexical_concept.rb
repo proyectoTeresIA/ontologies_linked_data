ddaaa9dd3fa8cb4abf8105766bcf6bc1949a3d23
markus-schuller <markus.schuller@librairy.eu>
2026-04-14 17:21:34 +0100

    Added inScheme expansion for LexicalConcept models

--- lib/ontologies_linked_data/models/ontolex/lexical_concept.rb
+++ lib/ontologies_linked_data/models/ontolex/lexical_concept.rb
@@ -155,10 +155,238 @@ module LinkedData
             end.compact
           end
 
+          # Expand inScheme -> ConceptSet -> dct:source -> LexicalConceptualResource
+          if concept.inScheme
+            expanded_schemes = normalize_to_array(concept.inScheme).map do |scheme|
+              expand_in_scheme_for_concept(scheme, submission)
+            end.compact
+
+            concept.inScheme = expanded_schemes.length == 1 ? expanded_schemes.first : expanded_schemes
+          end
+
           # Expand subject
           return unless concept.subject
 
           concept.subject = expand_subject_for_concept(concept.subject, submission)
+        end
+
+        # Helper class method to expand a ConceptSet from skos:inScheme
+        # and resolve its dct:source (LexicalConceptualResource)
+        def self.expand_in_scheme_for_concept(scheme_obj, submission)
+          if scheme_obj.is_a?(Hash)
+            return scheme_obj if scheme_obj['source']
+          end
+
+          scheme_uri_str = extract_uri_from_value(scheme_obj)
+          return scheme_obj.to_s unless scheme_uri_str && !scheme_uri_str.empty?
+
+          scheme_data = expand_auxiliary_entity(
+            scheme_uri_str,
+            submission,
+            'ConceptSet',
+            %w[source],
+            expand_refs: false,
+            strict: true
+          )
+
+          if !scheme_data.is_a?(Hash) || scheme_data['source'].nil?
+            conceptset_uri = find_concept_set_for_scheme(scheme_uri_str, submission)
+            if conceptset_uri
+              scheme_data = expand_auxiliary_entity(
+                conceptset_uri,
+                submission,
+                'ConceptSet',
+                %w[source],
+                expand_refs: false,
+                strict: true
+              )
+            end
+          end
+
+          if scheme_data.is_a?(Hash) && scheme_data['source']
+            scheme_data['source'] = expand_lexical_conceptual_resource(scheme_data['source'], submission)
+          elsif scheme_data.is_a?(Hash)
+            scheme_data['source'] = expand_lexical_conceptual_resource(scheme_uri_str, submission)
+          else
+            fallback_source = expand_lexical_conceptual_resource(scheme_uri_str, submission)
+            if fallback_source.is_a?(Hash)
+              scheme_data = {
+                '@id' => scheme_uri_str,
+                'source' => fallback_source
+              }
+            end
+          end
+
+          scheme_data
+        end
+
+        def self.find_concept_set_for_scheme(scheme_uri, submission)
+          graph = submission&.id
+          return nil if graph.nil?
+
+          epr = Goo.sparql_query_client(:main)
+          concept_set_types = [
+            'http://www.w3.org/ns/lemon/ontolex#ConceptSet',
+            'https://www.w3.org/ns/lemon/ontolex#ConceptSet'
+          ]
+
+          concept_set_types.each do |type_uri|
+            query = <<~SPARQL
+              SELECT ?conceptSet WHERE {
+                GRAPH <#{graph}> {
+                  ?conceptSet a <#{type_uri}> .
+                  ?conceptSet <http://purl.org/dc/terms/source> <#{scheme_uri}> .
+                }
+              }
+              LIMIT 1
+            SPARQL
+            results = epr.query(query)
+            return results.first[:conceptSet].to_s if results && !results.empty?
+          end
+
+          nil
+        rescue StandardError => e
+          puts "[LexicalConcept] Error resolving ConceptSet for scheme #{scheme_uri}: #{e.message}"
+          nil
+        end
+
+        def self.extract_uri_from_value(value)
+          return nil if value.nil?
+          return value.to_s if value.is_a?(RDF::URI) || value.is_a?(String)
+          return value.id.to_s if value.respond_to?(:id) && value.id
+
+          if value.is_a?(Hash)
+            return value['@id'] if value['@id']
+            return value['id'] if value['id']
+            return value['uri'] if value['uri']
+          end
+
+          if value.is_a?(Array)
+            return value[1].to_s if value.length == 2 && value[0].to_s == 'uri'
+            return extract_uri_from_value(value.first)
+          end
+
+          value.to_s
+        end
+
+        def self.normalize_to_array(value)
+          return [] if value.nil?
+          return value if value.is_a?(Array)
+
+          [value]
+        end
+
+        # Expand a Meta-Share LexicalConceptualResource entity referenced from ConceptSet dct:source
+        def self.expand_lexical_conceptual_resource(resource_obj, submission)
+          return resource_obj.to_s unless resource_obj.is_a?(RDF::URI) || resource_obj.is_a?(String)
+
+          predicate_overrides = {
+            'resourceName' => 'http://w3id.org/meta-share/meta-share/2.0.0/resourceName',
+            'resourceCreator' => 'http://w3id.org/meta-share/meta-share/2.0.0/resourceCreator',
+            'language' => 'http://w3id.org/meta-share/meta-share/2.0.0/language',
+            'domain' => 'http://w3id.org/meta-share/meta-share/2.0.0/domain',
+            'uri' => 'http://w3id.org/meta-share/meta-share/2.0.0/uri'
+          }
+
+          candidates = candidate_resource_uris(resource_obj, submission)
+          result = nil
+
+          candidates.each do |candidate_uri|
+            result = expand_auxiliary_entity(
+              candidate_uri,
+              submission,
+              'LexicalConceptualResource',
+              %w[resourceName uri resourceCreator language domain],
+              expand_refs: false,
+              predicate_overrides: predicate_overrides,
+              multivalued_fields: %w[resourceCreator language domain],
+              normalize_language_codes: false,
+              strict: true
+            )
+            break if result.is_a?(Hash) && result['resourceName']
+          end
+
+          return resource_obj.to_s unless result
+
+          if result.is_a?(Hash) && result['resourceCreator']
+            creators = Array(result['resourceCreator'])
+            expanded_creators = creators.map { |creator| expand_actor(creator, submission) }
+            result['resourceCreator'] = expanded_creators
+          end
+
+          result
+        end
+
+        def self.candidate_resource_uris(resource_obj, submission)
+          source_uri = resource_obj.to_s
+          candidates = [source_uri]
+
+          begin
+            uri_match = source_uri.match(%r{\A(https?://[^/]+)(/.*)?\z})
+            if uri_match
+              base = uri_match[1]
+              path = uri_match[2].to_s
+              last_segment = path.split('/').reject(&:empty?).last
+              candidates << "#{base}/resources/#{last_segment}" if last_segment && !last_segment.empty?
+            end
+          rescue StandardError
+          end
+
+          by_local_name = find_resource_by_local_name(source_uri, submission)
+          candidates << by_local_name if by_local_name
+
+          candidates.compact.uniq
+        end
+
+        def self.find_resource_by_local_name(source_uri, submission)
+          graph = submission&.id
+          return nil if graph.nil?
+
+          local_name = source_uri.to_s.split(/[\/#]/).last
+          return nil if local_name.nil? || local_name.empty?
+
+          escaped_local_name = local_name.gsub('"', '\\"')
+          epr = Goo.sparql_query_client(:main)
+          type_uris = [
+            'http://w3id.org/meta-share/meta-share/2.0.0/LexicalConceptualResource',
+            'http://w3id.org/meta-share/meta-share/LexicalConceptualResource',
+            'https://w3id.org/meta-share/meta-share/2.0.0/LexicalConceptualResource',
+            'https://w3id.org/meta-share/meta-share/LexicalConceptualResource'
+          ]
+
+          type_uris.each do |type_uri|
+            query = <<~SPARQL
+              SELECT ?res WHERE {
+                GRAPH <#{graph}> {
+                  ?res a <#{type_uri}> .
+                  FILTER(STRENDS(STR(?res), "#{escaped_local_name}"))
+                }
+              }
+              LIMIT 1
+            SPARQL
+
+            results = epr.query(query)
+            return results.first[:res].to_s if results && !results.empty?
+          end
+
+          nil
+        rescue StandardError => e
+          puts "[LexicalConcept] Error resolving LexicalConceptualResource by local name #{source_uri}: #{e.message}"
+          nil
+        end
+
+        # Expand Meta-Share Actor label for resourceCreator references
+        def self.expand_actor(actor_obj, submission)
+          return actor_obj.to_s unless actor_obj.is_a?(RDF::URI) || actor_obj.is_a?(String)
+
+          expand_auxiliary_entity(
+            actor_obj,
+            submission,
+            'Actor',
+            %w[label],
+            expand_refs: false,
+            strict: true
+          )
         end
 
         # Helper class method to expand a Definition object
@@ -235,10 +463,18 @@ module LinkedData
         end
 
         # Generic helper to expand any auxiliary entity
-        def self.expand_auxiliary_entity(obj, submission, entity_type, fields, expand_refs: true)
+        def self.expand_auxiliary_entity(obj, submission, entity_type, fields, expand_refs: true,
+                                         predicate_overrides: {}, multivalued_fields: [],
+                                         normalize_language_codes: true, strict: false)
           if obj.is_a?(RDF::URI) || obj.is_a?(String)
             uri = RDF::URI.new(obj.to_s)
-            graph = submission.id
+            graph = submission&.id
+            if strict && graph.nil?
+              raise ArgumentError, "[LexicalConcept] Missing submission graph while loading #{entity_type} #{uri}"
+            end
+
+            return obj.to_s if graph.nil?
+
             epr = Goo.sparql_query_client(:main)
 
             begin
@@ -259,7 +495,7 @@ module LinkedData
                   'signedRep' => 'https://w3id.org/def/easytv#signedRep',
                   'url' => 'https://w3id.org/def/easytv#url'
                 }
-                pred = ns_map[field] || field
+                pred = predicate_overrides[field] || ns_map[field] || field
                 "OPTIONAL { <#{uri}> <#{pred}> ?#{field} . }"
               end.join(' ')
 
@@ -276,9 +512,11 @@ module LinkedData
                   next if all_vals.empty?
 
                   # Handle language URIs (lexvo)
-                  if field == 'language' && all_vals.first.to_s.start_with?('http://lexvo.org/id/iso639')
+                  effective_multivalued = %w[wasDerivedFrom hasDerivation influenced wasAssociatedFor source] + multivalued_fields
+
+                  if normalize_language_codes && field == 'language' && all_vals.first.to_s.start_with?('http://lexvo.org/id/iso639')
                     result[field] = all_vals.map { |v| v.to_s.split('/').last }.first
-                  elsif %w[wasDerivedFrom hasDerivation influenced wasAssociatedFor source].include?(field)
+                  elsif effective_multivalued.include?(field)
                     # These can be multi-valued
                     result[field] = all_vals.size > 1 ? all_vals.map(&:to_s) : all_vals.first.to_s
                   else
@@ -307,7 +545,11 @@ module LinkedData
                 return result
               end
             rescue StandardError => e
-              puts "[LexicalConcept] Error loading #{entity_type} #{uri}: #{e.message}"
+              if strict
+                raise StandardError, "[LexicalConcept] Error loading #{entity_type} #{uri}: #{e.message}"
+              else
+                puts "[LexicalConcept] Error loading #{entity_type} #{uri}: #{e.message}"
+              end
             end
           end
 
